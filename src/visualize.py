5c370f097c650b5df1bb844fdb8c795bb36e5e4e
J-Rojas <jrojas@redlinesoftworks.com>
2025-01-31 17:57:38 -0800

    Add visualization of airport runways

--- src/visualize.py
+++ src/visualize.py
@@ -6,6 +6,7 @@ from pathlib import Path
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
+import matplotlib.lines as mlines
 from src.detector import read_data
 from src.geo_utils import get_points_near
 import os
@@ -18,7 +19,7 @@ from shapely import Polygon, Point
 from datetime import datetime
 from IPython.display import Image, display
 
-def plot_charts(name: str, df: pd.DataFrame, takeoff_ind: int, runway: tuple[Polygon, float], save_func=lambda fig, name: fig.savefig(f"plots/plot_{name}.png",  bbox_inches='tight')):
+def plot_charts(name: str, df: pd.DataFrame, takeoff_ind: int, runway: tuple[Polygon, float]|None, save_func=lambda fig, name: fig.savefig(f"plots/plot_{name}.png",  bbox_inches='tight')):
 
     # Iterate through each row in the DataFrame
     # Create a new figure for each row
@@ -56,30 +57,46 @@ def plot_charts(name: str, df: pd.DataFrame, takeoff_ind: int, runway: tuple[Pol
         axes[2].plot(df.index[takeoff_ind], df[CSVColumns.AltMSL][takeoff_ind], "go", label='Takeoff Moment')
     axes[2].legend(loc='upper right')  # Explicitly set the legend location
 
-    # Plot the flight, zooming into the runway
-    runway_poly = runway[0]
-    centroid = runway_poly.centroid
-    gps_points = list(map(lambda x: Point(x[1], x[0]), zip(df[CSVColumns.Latitude], df[CSVColumns.Longitude])))[:takeoff_ind+200]
-    points_near_centroid = get_points_near(centroid, 0.01, gps_points)[::5]
-    url = generate_static_map_url(centroid.y, centroid.x, poly=points_near_centroid)
+    if runway:
 
-    # Open the URL and read the image
-    with urllib.request.urlopen(url) as response:
-        img_data = response.read()
+        marker_data = None
+        if takeoff_ind != -1:
+            marker_data = {
+                'location': f"{df[CSVColumns.Latitude][takeoff_ind]},{df[CSVColumns.Longitude][takeoff_ind]}",
+                'color': "0xFF00FF",
+                'label': ''
+            }
 
-    # Convert the response data to a Pillow Image
-    img = image_open.open(io.BytesIO(img_data))
+        # Plot the flight, zooming into the runway
+        runway_poly = runway[0]
+        centroid = runway_poly.centroid
+        gps_points = list(map(lambda x: Point(x[1], x[0]), zip(df[CSVColumns.Latitude], df[CSVColumns.Longitude])))[:takeoff_ind+200]
+        points_near_centroid = get_points_near(centroid, 0.015, gps_points)[::5]
 
-    # Convert the Pillow Image to a numpy array
-    img_array = np.array(img)
+        flight_path_config = {
+            "color": "0xFF00FF80",
+            "points": points_near_centroid
+        }
 
-    axes[3].imshow(img)
-    axes[3].set_title("Runway")
-    axes[3].axis('off')
+        url = generate_static_map_url(centroid.y, centroid.x, poly=[flight_path_config], marker_config=marker_data)
 
-    # --- Add Legend to Subplot ---
-    legend_patch = mpatches.Patch(color="magenta", label="flight path") # Create patch for THIS subplot
-    axes[3].legend(handles=[legend_patch], labels=[legend_patch.get_label()], loc='upper right', frameon=False) # Add legend to the AXES, remove frame
+        # Open the URL and read the image
+        with urllib.request.urlopen(url) as response:
+            img_data = response.read()
+
+        # Convert the response data to a Pillow Image
+        img = image_open.open(io.BytesIO(img_data))
+
+        axes[3].imshow(img)
+        axes[3].set_title("Runway")
+        axes[3].axis('off')
+
+        # --- Add Legend to Subplot ---
+        legend_patch = mpatches.Patch(color="magenta", label="flight path") # Create patch for THIS subplot
+        marker_legend_line = mlines.Line2D([], [], color="magenta", marker='o', linestyle='None',
+                                markersize=10, label="Takeoff Moment") # Line2D for marker representation
+
+        axes[3].legend(handles=[legend_patch, marker_legend_line], labels=[legend_patch.get_label(), marker_legend_line.get_label()], loc='upper right', frameon=False) # Add legend to the AXES, remove frame
 
     # Adjust layout for better spacing
     plt.tight_layout()
@@ -88,21 +105,34 @@ def plot_charts(name: str, df: pd.DataFrame, takeoff_ind: int, runway: tuple[Pol
 
     plt.close(fig)  # Close the figure to avoid display in the notebook
 
-def generate_static_map_url(center_lat, center_lng, zoom=16, size="600x400", map_type="satellite", poly=None):
+def generate_static_map_url(center_lat, center_lng, zoom=15, size="600x400", map_type="satellite", poly:list[dict]|None=None, marker_config=None):
     base_url = "https://maps.googleapis.com/maps/api/staticmap"
-    params = {
-        "center": f"{center_lat},{center_lng}",
-        "zoom": zoom,
-        "size": size,
-        "maptype": map_type,
-        "key": os.getenv('GOOGLE_MAPS_API_KEY')
-    }
+    params = [
+        ("center", f"{center_lat},{center_lng}"),
+        ("zoom", zoom),
+        ("size", size),
+        ("maptype", map_type),
+        ("key", os.getenv('GOOGLE_MAPS_API_KEY'))
+    ]
+
+    if marker_config and marker_config['location']:
+        marker_style = f"color:{marker_config['color']}|size:3|label:{marker_config['label']}|"
+        params.append(('markers', marker_style + marker_config['location']))
+
+    query_string = urllib.parse.urlencode(
+        params,
+        safe="|:,",  # Do not URL-encode these characters
+        quote_via=urllib.parse.quote
+    )
 
     if poly:
-        polyl = "color:0xFF00FF80|weight:2" + "|".join(list(map(lambda p: f"{p.y},{p.x}", poly)))
-        params["path"] = polyl
+        for pline in poly:
+            color = pline['color']
+            points = pline['points']
+            polyl = f"color:{color}|weight:3|" + "|".join(list(map(lambda p: f"{p.y},{p.x}", points)))
+            query_string += f"&path={polyl}"
 
-    query_string = urllib.parse.urlencode(params)
+
     return f"{base_url}?{query_string}"
 
 def display_static_map(url, size="400x400"):
@@ -120,4 +150,4 @@ if __name__ == "__main__":
         takeoff_df, runways = read_data(Path(f"data/cessna_182t/{row[0]}"))
 
         takeoff_ind = row[2]
-        plot_charts(row[0], takeoff_df, takeoff_ind)
+        plot_charts(row[0], takeoff_df, runways[row[1]], takeoff_ind)
